942fc666e78edaf3a4f53fcb9c1f9f47481dffc4
Matt Chowning <mchowning@gmail.com>
2015-01-05 22:23:49 -0500

    Updating Exercise 1 on hw2

--- src/cis194/Hw2.hs
+++ src/cis194/Hw2.hs
@@ -2,39 +2,60 @@
 module Hw2 where
 
 import Hw2Log
+import Test.HUnit
+
+--- Exercise 1
 
 parseMessage :: String -> LogMessage
-parseMessage s = LogMessage messageType timestamp message
+parseMessage s = parseHelper $ words s
   where
-    ls = words s
-    messageType = getMessageType ls
-    timestamp = getTimeStamp ls
-    message = getMessage ls
-
-getMessageType :: [String] -> MessageType
-getMessageType ("E":l:_) = Error (read l)
-getMessageType ("I":_)   = Info
-getMessageType ("W":_)   = Warning
-getMessageType _         = undefined
-
-getTimeStamp :: [String] -> TimeStamp
-getTimeStamp = read . head . removeMessageType
-
-getMessage :: [String] -> String
-getMessage = unwords . drop 1 . removeMessageType
-
-removeMessageType :: [String] -> [String]
-removeMessageType xs@("E":_) = drop 2 xs
-removeMessageType xs         = drop 1 xs
-
+    parseHelper :: [String] -> LogMessage
+    parseHelper ("E":i:t:m) = LogMessage (Error (read i)) (read t) (unwords m)
+    parseHelper ("I":t:m)   = LogMessage Info (read t) (unwords m)
+    parseHelper ("W":t:m)   = LogMessage Warning (read t) (unwords m)
+    parseHelper _           = Unknown s
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
-iTest :: String
-iTest = "I 6 Completed armadillo processing"
-wTest :: String
-wTest = "W 5 Flange is due for a check-up"
-eTest :: String
-eTest = "E 20 2 Too many pickles"
+runParseMessageTests :: IO Counts
+runParseMessageTests = runTestTT allTests
+  where
+    allTests = TestList $ map baseTest testCases
+    baseTest (input, result) = TestCase $ assertEqual "" result (parseMessage input)
+    testCases = [ ("I 6 Completed armadillo processing", LogMessage Info 6 "Completed armadillo processing")
+                , ("W 5 Flange is due for a check-up",   LogMessage Warning 5 "Flange is due for a check-up")
+                , ("E 20 2 Too many pickles",            LogMessage (Error 20) 2 "Too many pickles")
+                , ("abcdef",                             Unknown "abcdef") ]
 
+
+--- Exercise 2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree                                         = tree
+insert lm Leaf                                                  = Node Leaf lm Leaf
+insert lm@(LogMessage _ ts _) (Node lt (LogMessage _ tts _) rt) | ts <= tts = insert lm lt
+                                                                | ts > tts  = insert lm rt
+insert _ _                                                      = undefined
+
+-- runInsertTests = runTestTT insertTests
+--   where
+--     insertTests = TestList $ map baseTest testCases
+--     baseTest (input1, input2) result = TestCase $ assertEquals "" result (insert input1 input2)
+--     testCases = [ (LogMessage]
+
+
+lmg :: Int -> LogMessage
+lmg n = LogMessage Info n ""
+
+sampleTree :: MessageTree
+sampleTree = Node
+              (Node
+                Leaf
+                (lmg 10)
+                Leaf)
+              (lmg 20)
+              (Node
+                Leaf
+                (lmg 30)
+                Leaf)
