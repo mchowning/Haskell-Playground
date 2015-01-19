0334713339551e87297b3141f6857d5cca8b5709
Matt Chowning <mchowning@gmail.com>
2015-01-19 15:16:30 -0500

    Added hw5, ex4

--- src/cis194/hw5.hs
+++ src/cis194/hw5.hs
@@ -45,3 +45,39 @@ instance Expr ExprT where
 
 exprClassTest :: Test
 exprClassTest = (mul (add (lit 2) (lit 3)) (lit 4) :: ExprT) ~?= Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+
+
+--- Exercise 4
+
+
+instance Expr Integer where
+  lit a   = a
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr Bool where
+  lit a | a > 0     = True
+        | otherwise = False
+  add a b = a || b
+  mul a b = a && b
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit                       = MinMax
+  add (MinMax a) (MinMax b) = MinMax (max a b)
+  mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit a = Mod7 (mod a 7)
+  add (Mod7 a) (Mod7 b) = Mod7 $ mod (a + b) 7
+  mul (Mod7 a) (Mod7 b) = Mod7 $ mod (a * b) 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+instanceTests = TestList [ (testExp :: Maybe Integer) ~?= Just (-7)
+                         , (testExp :: Maybe Bool)    ~?= Just True
+                         , (testExp :: Maybe MinMax)  ~?= Just (MinMax 5)
+                         , (testExp :: Maybe Mod7)    ~?= Just (Mod7 0)
+                         ]
