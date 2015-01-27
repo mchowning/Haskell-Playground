b3902fb7f500e3e2cfe1d3940c65240b29fc1c8c
Matt Chowning <mchowning@gmail.com>
2015-01-27 13:41:52 -0500

    Cleaned up hw1 a big

--- src/courses/algorithms1/hw1.hs
+++ src/courses/algorithms1/hw1.hs
@@ -2,33 +2,16 @@
 
 import Test.HUnit
 
+-- todo: try using Data.Text instead of String
+
 -- result is 2407905288
 
 inversionFinder :: IO ()
 inversionFinder = do contents <- readFile "IntegerArray.txt"
-                     print . countInversions . convertToListOfIntegers $ contents
+                     print . fst . findInversions . convertToListOfIntegers $ contents
   where
     convertToListOfIntegers :: String -> [Int]
     convertToListOfIntegers = map read . lines
-
-------------------------------------------------------------------------------------------------------------------------
-
-countInversions :: (Show a, Ord a) => [a] -> Int
-countInversions = fst . findInversions
-
-countInversionsTest :: Test
-countInversionsTest = TestList
-  [ countInversions ""    ~?= 0
-  , countInversions "a"   ~?= 0
-  , countInversions "ba"  ~?= 1
-  , countInversions "ab"  ~?= 0
-  , countInversions "cba" ~?= 3
-  , countInversions "acb" ~?= 1
-  , countInversions input ~?= 2372 ]
-    where
-      input=[4,80,70,23,9,60,68,27,66,78,12,40,52,53,44,8,49,28,18,46,21,39,51,7,87,99,69,62,84,6,79,67,14,98,83,0,96,5,82,10
-            ,26,48,3,2,15,92,11,55,63,97,43,45,81,42,95,20,25,74,24,72,91,35,86,19,75,58,71,47,76,59,64,93,17,50,56,94,90,89
-            ,32,37,34,65,1,73,41,36,57,77,30,22,13,29,38,16,88,61,31,85,33,54] :: [Int]
 
 ------------------------------------------------------------------------------------------------------------------------
 
