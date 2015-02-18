44e8bc5d10f9a56a7a9c5eef3132d528a0c2758a
Matt Chowning <mchowning@gmail.com>
2015-02-18 07:27:48 -0500

    ImportSorter cleanup

--- src/hw/import_parser/ImportSorter.hs
+++ src/hw/import_parser/ImportSorter.hs
@@ -9,58 +9,56 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.CaseInsensitive as CI
 
+
 main :: IO ()
 main = do
     filename <- head <$> getArgs
     contents <- T.readFile filename
+    let (preImportLines, sortedImportLines, postImportLines) = separateImportLines contents
+--     writeFile "testOutput.txt" $ T.unpack . T.unlines $ preImportLines ++ sortedImportLines ++ ("\n" : postImportLines)
 
-    let (preImportLines, importLines, postImportLines) = separateImportLines contents
-    let sortedImports = sortImports importLines
 
     -- todo remove
     T.putStrLn "PREIMPORT LINES:"
     mapM_ T.putStrLn preImportLines
     T.putStrLn "IMPORT LINES:"
-    mapM_ T.putStrLn sortedImports
+    mapM_ T.putStrLn sortedImportLines
     T.putStrLn "POST IMPORT LINES:"
     mapM_ T.putStrLn postImportLines
     -----------------------------------------------------------
 
-    let newFileContents = T.unpack . T.unlines $ preImportLines ++ sortedImports ++ (T.singleton '\n' : postImportLines)
-
+    let newFileContents = T.unpack . T.unlines $ preImportLines ++ sortedImportLines ++ ("\n" : postImportLines)
     writeFile "testOutput.txt" newFileContents
---     writeFile filename newFileContents
     -- todo uncomment real line
 
+separateImportLines :: T.Text -> ([T.Text], [T.Text], [T.Text])
+separateImportLines t = (preImportLines, sortImports importLines, postImportLines)
   where
-    separateImportLines :: T.Text -> ([T.Text], [T.Text], [T.Text])
-    separateImportLines t = (preImportLines, importLines, postImportLines)
-      where
-        (preImportLines, rest) = break (isImportLine False) (T.lines t)
-        (importLines, postImportLines) = span (isImportLine True) rest
+    (preImportLines, rest) = break (isImportLine False) (T.lines t)
+    (importLines, postImportLines) = span (isImportLine True) rest
 
-        isImportLine :: Bool -> T.Text -> Bool
-        isImportLine onBlank "" = onBlank
-        isImportLine onBlank aLine | isBlankLine aLine = onBlank
-                                   | otherwise         = "import" == head (T.words aLine)
+    isImportLine :: Bool -> T.Text -> Bool
+    isImportLine onBlank "" = onBlank
+    isImportLine onBlank aLine | isBlankLine aLine = onBlank
+                               | otherwise         = "import" == head (T.words aLine)
 
-    sortImports :: [T.Text] -> [T.Text]
-    sortImports = sortBy orderCaseInsensitiveSecondElement . removeEmptyLines
+sortImports :: [T.Text] -> [T.Text]
+sortImports = sortBy orderCaseInsensitiveSecondElement . removeEmptyLines
+  where
+    orderCaseInsensitiveSecondElement :: T.Text -> T.Text -> Ordering
+    orderCaseInsensitiveSecondElement = comparing (CI.mk . getSecondElement)
       where
-        orderCaseInsensitiveSecondElement :: T.Text -> T.Text -> Ordering
-        orderCaseInsensitiveSecondElement = comparing (CI.mk . getSecondElement)
+        getSecondElement :: T.Text -> T.Text
+        getSecondElement fullLine | length ws > 1 = ws !! 1
+                                  | otherwise     = ""
           where
-            getSecondElement :: T.Text -> T.Text
-            getSecondElement fullLine | length ws > 1 = ws !! 1
-                                      | otherwise     = ""
-              where
-                ws = T.words fullLine
+            ws = T.words fullLine
 
-        removeEmptyLines :: [T.Text] -> [T.Text]
-        removeEmptyLines = filter (not . isBlankLine)
+    removeEmptyLines :: [T.Text] -> [T.Text]
+    removeEmptyLines = filter (not . isBlankLine)
 
-    isBlankLine :: T.Text -> Bool
-    isBlankLine = T.foldr (\i acc -> acc && (i == ' ' || i == '\n')) True
+isBlankLine :: T.Text -> Bool
+isBlankLine = T.null . T.strip
 
 
 
